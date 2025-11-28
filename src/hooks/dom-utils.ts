95b3b7999675beb61c97dcf4676d1e614e317438
jniac <joseph.merdrignac@gmail.com>
2025-11-28 09:29:05 +0100

    useTitle() react to "focus" events

--- src/hooks/dom-utils.ts
+++ src/hooks/dom-utils.ts
@@ -15,7 +15,12 @@ export function useTitle(title: string) {
   useEffect(() => {
     const previousTitle = document.title
     document.title = title
+    const onFocus = () => {
+      document.title = title
+    }
+    window.addEventListener('focus', onFocus)
     return () => {
+      window.removeEventListener('focus', onFocus)
       document.title = previousTitle
     }
   }, [title])
