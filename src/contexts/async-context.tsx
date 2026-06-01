f03b2808012b5c09312130e1121ae2c7bb0e9284
jniac <joseph.merdrignac@gmail.com>
2026-06-01 09:41:12 +0200

    createAsyncContext() has now an "invalidate" function

--- src/contexts/async-context.tsx
+++ src/contexts/async-context.tsx
@@ -1,36 +1,60 @@
 'use client'
 
-import { createContext, useContext, useEffect, useState } from 'react'
+import { createContext, useContext, useEffect, useRef, useState } from 'react'
+
+type InvalidateOptions = {
+  cacheMaxAge?: number
+}
+
+type InvalidateFunction = (options?: InvalidateOptions) => void
 
 export function createAsyncContext<T>(
   initializer: () => Promise<T>
 ): [
     Provider: React.FC<{ children?: React.ReactNode }>,
-    useContext: () => T,
+    useContext: () => [context: T, invalidate: InvalidateFunction],
   ] {
-  const deferredContext = createContext<T>(null!)
+  const deferredContext = createContext<[T, InvalidateFunction]>(null!)
   const Provider = ({ children }: { children?: React.ReactNode }) => {
+    const [timestamp, setTimestamp] = useState(Date.now())
+    const timestampRef = useRef(0)
+
+    const invalidate: InvalidateFunction = (options) => {
+      const now = Date.now()
+      if (options?.cacheMaxAge) {
+        const cacheAge = now - timestampRef.current
+        if (cacheAge < options.cacheMaxAge) {
+          return
+        }
+      }
+      timestampRef.current = now
+      setTimestamp(now)
+    }
+
     const [value, setValue] = useState<T | null>(null)
     useEffect(() => {
       initializer().then(value => {
+        timestampRef.current = timestamp
         setValue(value)
       })
-    }, [])
+    }, [timestamp])
+
     if (value === null) {
       return null
     }
+
     return (
-      <deferredContext.Provider value={value}>
+      <deferredContext.Provider value={[value, invalidate]}>
         {children}
       </deferredContext.Provider>
     )
   }
   const useDeferredContext = () => {
-    const value = useContext(deferredContext)
-    if (value === null) {
+    const context = useContext(deferredContext)
+    if (context === null) {
       throw new Error('Deferred context not initialized')
     }
-    return value
+    return context
   }
   return [Provider, useDeferredContext] as const
 }
